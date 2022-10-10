23ba6b886e31b2349202c3ecde0bee19fae1df8e
zhangjunjie <11111111>
2022-10-10 22:44:27 +0800

    修改了api层的接口字段

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 axios.defaults.baseURL='http://192.168.0.104:3000/'
 export const getTreeList=()=>{
-    return axios.get('/getTreeList')
+    return axios.get('/tree/getTreeList')
 }
 
 export const deleteTreeList=(id)=>{
-    return axios.get('/deleteTreeList/'+id)
+    return axios.get('/tree/deleteTreeList/'+id)
 }
